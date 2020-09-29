025294f9758a2603b13d35b8a437ba3cd5924eb6
Arista Indrajaya <arista.indrajaya@gmail.com>
2020-09-29 21:52:54 +0700

    restructured

--- learn-java/halo/src/Halo.java
+++ learn-java/halo/src/Halo.java
@@ -3,20 +3,21 @@ package belajar.java.fundamental;
 import java.util.Date;
 import training.internet.education.Siswa;
 
-public class Halo{
+public class Halo {
     private Date sekarang = new Date();
 
     public void sapa(String nama){
-        System.out.println("Hallo" + nama);
+        System.out.println("Halo " + nama);
     }
 
-    public void tampilkanWaktu(){
+    public void tampilWaktu(){
         System.out.println("Waktu sekarang : " + sekarang);
     }
 
     public static void main(String[] x){
-        Siswa s = new Siswa("Rudi");
-        System.out.println("Menjalankan method main");
-        System.out.println("Halo "+ s.getNama());
+        System.out.println("Memuat main method");
+
+        Siswa s = new Siswa("Rudiansyah");
+        System.out.println("Halo "+s.getNama());
     }
 }
